edaeafa442892e4821ccb342e17d2cb831c34323
Faizal Hamzah <reinmclaren33@gmail.com>
2023-07-06 19:07:24 +0700

    Change Generate EPG configuration

--- epg/scripts/index.py
+++ epg/scripts/index.py
@@ -64,7 +64,7 @@ def main():
     print("Parsing data...")
     tostring = et.tostring(tree, encoding="UTF-8", method="xml", xml_declaration=True)
     parsestring = dom.parseString(tostring).toprettyxml(indent='', encoding="UTF-8")
-    output = re.sub(b'\n', b'', parsestring)
+    output = re.sub(b'\n\n', b'', parsestring)
 
     print("Creating file...")
     with open(result, mode="wb") as epg:
